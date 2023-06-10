2548903b5f82ceed22bb5777e8518660df502ebd
Tellagorla Manohar <113425829+manohar2207@users.noreply.github.com>
2023-06-10 07:29:42 +0530

    Create Interest.java

---
+++ Interest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.assertTrue;
+
+import java.util.Scanner;
+
+class intrest 
+
+{
+
+	public static void main(String[] args)	{ 
+
+		Scanner sc=new Scanner(System.in);
+
+		System.out.println("Simple interest = ");
+
+		float P=sc.nextFloat(); 
+
+		float R=sc.nextFloat(); 
+
+		float T=sc.nextFloat(); 
+
+		float SI = (P * T * R) / 100; 
+
+		System.out.println("Simple interest = " + SI);
+
+		assertTrue(72==SI);
+
+		sc.close();
+
+		}
+
+	}
